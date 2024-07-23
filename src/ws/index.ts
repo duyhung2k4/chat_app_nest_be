09a77a67c952bc64f4b09505530f47606d528633
ndhung <nguyenduyhung04092004@gmail.com>
2024-07-24 02:44:53 +0700

    .

--- src/ws/index.ts
+++ src/ws/index.ts
@@ -7,11 +7,24 @@ class ServerSocket {
     Init() {
         this.wss = new WebSocketServer({ port: Number(8080) });
         this.wss.on("connection", (ws: WebSocket, req: IncomingMessage) => {
+            let mapParams = {};
+            req.url.split("?")[1].split("&").forEach(item => {
+                const [key, value] = item.split("=");
+                mapParams[key] = value;
+            });
+
+            ws.on("message", (data) => {
+                const { mess } = JSON.parse(data.toString());
+                ws.send(JSON.stringify({
+                    rep: mess,
+                }))
+            })
+
             ws.on("error", (err) => {
                 console.log(err);
             });
             ws.on("close", () => {
-                console.log(req.headers);
+                // console.log(req.headers.connection);
             });
         });
     }
